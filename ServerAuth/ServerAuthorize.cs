0555c4467722d74d879fd128904e2a8911a1160a
Mr. BigMax <maxx2223@yandex.ru>
2016-02-13 10:08:44 +0300

    send login on server

---
+++ ServerAuth/ServerAuthorize.cs
@@ -0,0 +1,76 @@
+﻿// SocketServer.cs
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using MySql;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Устанавливаем для сокета локальную конечную точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            // Создаем сокет Tcp/Ip
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Начинаем слушать соединения
+                while (true)
+                {
+                    Console.WriteLine("Ожидаем соединение через порт {0}", ipEndPoint);
+
+                    // Программа приостанавливается, ожидая входящее соединение
+                    Socket handler = sListener.Accept();
+                    string login = null;
+
+                    // Мы дождались клиента, пытающегося с нами соединиться
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+
+                    login += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                    // Показываем данные на консоли
+                    Console.Write("Логин клиента: " + login + "\n\n");
+
+                    // Отправляем ответ клиенту\
+                    string reply = "Спасибо, ваш логин << " + login
+                            + " >> добавлен в базу!";
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    handler.Send(msg);
+
+                    if (login.IndexOf("Exit") > -1)
+                    {
+                        Console.WriteLine("Сервер завершил соединение с клиентом.");
+                        break;
+                    }
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
